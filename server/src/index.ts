64650b212727eac29dc81b023488add91db81a8a
B <bkashishh07@gmail.com>
2024-09-03 23:28:02 +0530

    added code for checking the if store has sessionid

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import redisClient from "./redisclient/client";
 
 import { v4 as uuidv4 } from "uuid";
 
@@ -10,6 +11,7 @@ import { randomUUID } from "crypto";
 import { Socket } from "socket.io";
 import { userInfo } from "os";
 import router from "./routes/fileupload";
+import { disconnect } from "process";
 
 interface customsocket extends Socket {
   sessionID: string;
@@ -48,40 +50,69 @@ interface CustomSocket extends Socket {
   userID: string;
 }
 
+// io.use((socket: Socket, next) => {
+//   const customSocket = socket as CustomSocket;
+
+//   const sessionID = customSocket.handshake.auth.sessionID;
+
+//   const emailid = customSocket.handshake.auth.email;
+
+//   const name = customSocket.handshake.auth.name;
+
+//   console.log("custom handshake auth details noodu");
+
+//   console.log(customSocket.handshake.auth);
+//   if (sessionID) {
+//     const session = sessionStore.get(sessionID);
+
+//     if (session) {
+//       console.log("id ede marre ");
+//       console.log("session id exists");
+
+//       customSocket.sessionID = sessionID;
+//       customSocket.userID = session.userID;
+
+//       // socket.emit("idexist", { socket: socket, id: socket.id });
+
+//       return next();
+//     }
+//   }
+
+//   console.log("session id does not exist");
+//   const newSessionID = uuidv4();
+//   customSocket.sessionID = newSessionID;
+//   customSocket.userID = uuidv4();
+//   console.log(sessionID);
+//   console.log(customSocket.userID);
+//   sessionStore.set(customSocket.sessionID, {
+//     userID: customSocket.userID,
+//     customSocket,
+//   });
+
+//   customSocket.emit("session", {
+//     sessionID: customSocket.sessionID,
+//     userID: customSocket.userID,
+//   });
+
+//   next();
+// });
+
 io.use((socket: Socket, next) => {
   const customSocket = socket as CustomSocket;
-
   const sessionID = customSocket.handshake.auth.sessionID;
-
-  const emailid = customSocket.handshake.auth.email;
-
   const name = customSocket.handshake.auth.name;
 
-  console.log("custom handshake auth details noodu");
-
-  console.log(customSocket.handshake.auth);
-  if (sessionID) {
+  if (sessionID && sessionStore.has(sessionID)) {
     const session = sessionStore.get(sessionID);
-
-    if (session) {
-      console.log("id ede marre ");
-      console.log("session id exists");
-
-      customSocket.sessionID = sessionID;
-      customSocket.userID = session.userID;
-
-      // socket.emit("idexist", { socket: socket, id: socket.id });
-
-      return next();
-    }
+    customSocket.sessionID = sessionID;
+    customSocket.userID = session.userID;
+    return next();
   }
 
-  console.log("session id does not exist");
+  // Only create a new session if the session ID is missing or invalid
   const newSessionID = uuidv4();
   customSocket.sessionID = newSessionID;
   customSocket.userID = uuidv4();
-  console.log(sessionID);
-  console.log(customSocket.userID);
   sessionStore.set(customSocket.sessionID, {
     userID: customSocket.userID,
     customSocket,
@@ -95,9 +126,47 @@ io.use((socket: Socket, next) => {
   next();
 });
 
-io.on("connection", function (socket) {
+io.on("connection", async function (socket) {
   const customSocket = socket as CustomSocket;
   const existingre = socket.handshake.auth.sessionID;
+  const ssid = socket.handshake.auth.sessionID;
+  const name = socket.handshake.auth.name;
+  const phonenumber = socket.handshake.auth.phonenumber;
+  console.log(`name is ${name}`);
+  console.log(`my phonenumber is ${phonenumber}`);
+
+  // await redisClient.rPush(socket.id, customSocket);
+  // await redisClient.rPush(socket.id, customSocket);
+
+  // await redisClient.rPush(existingre, socket.id);
+  // const userdetails = JSON.parse({ name, phonenumber });
+  // const userdetails = JSON.stringify({ name, phonenumber });
+
+  // if (ssid) {
+  //   // i dont want to push the user to redis insted i need tu update or replace theat session with socket id and the name
+  // }
+  // await redisClient.rPush(
+  //   "new user",
+  //   JSON.stringify({ id: socket.id, name, phonenumber, existingre })
+  // );
+
+  if (!ssid) {
+    console.error("Session ID is missing, cannot set value in Redis.");
+    return;
+  }
+
+  const userDetailss = JSON.stringify({
+    name: name,
+    socketid: socket.id,
+    phonenumber: phonenumber,
+  });
+
+  try {
+    await redisClient.SET(ssid, userDetailss);
+    console.log("stored in redis");
+  } catch (e) {
+    console.log(e);
+  }
 
   console.log("a user connected");
   console.log(socket.id);
@@ -127,6 +196,10 @@ io.on("connection", function (socket) {
     console.log("someone sent the messages");
     console.log(`messages is ${message}`);
     console.log(to);
+
+    socket.on("disconnect", async function () {
+      await redisClient.lRem("newuser", 0, socket.id);
+    });
 
     const recipeentuser = userdetails.get(to);
 
